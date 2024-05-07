d3729a9562b32f165788fa28a3c395e112f87cdb
Nour BH <benhajlanour2@gmail.com>
2024-05-07 16:32:30 +0100

    mongoDB done

--- stream/src/main/java/com/example/ConsumerKafka.java
+++ stream/src/main/java/com/example/ConsumerKafka.java
@@ -1,40 +1,20 @@
 package com.example;
 
-
+import org.apache.spark.api.java.function.VoidFunction2;
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.api.java.UDF2;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.Trigger;
 import org.apache.spark.sql.types.DataTypes;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-import com.mongodb.client.MongoClient;
 import static org.apache.spark.sql.functions.*;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-
 
 
 
 public class ConsumerKafka {
 
-        // public static String sentimentAnalysis(int neg_rev_word_count, int pos_rev_word_count) {
-        //         if (neg_rev_word_count - pos_rev_word_count > 50)
-        //             return "Negative";
-        //         else if (neg_rev_word_count - pos_rev_word_count < -50)
-        //             return "Positive";
-        //         else
-        //             return "Neutral";
-        //     }
+     
         
-        
-
     public static void main(String[] args) throws Exception {
 
         if (args.length < 3) {
@@ -52,14 +32,16 @@ public class ConsumerKafka {
                 .master("local[*]")  // Set the master URL for local mode 
                 .config("spark.sql.streaming.checkpointLocation", "/checkpoint/")
                 .getOrCreate();
+       
+       
 
         // Register UDF
         UDFRegistration udf = spark.udf();
         udf.register("sentimentAnalysis", (UDF2<Integer, Integer, String>) 
                 (neg_rev_word_count, pos_rev_word_count) -> {
-                    if (neg_rev_word_count - pos_rev_word_count > 50)
+                    if (neg_rev_word_count - pos_rev_word_count > 50 || pos_rev_word_count == 0  )
                         return "Negative";
-                    else if (neg_rev_word_count - pos_rev_word_count < -50)
+                    else if (neg_rev_word_count - pos_rev_word_count < -50 || neg_rev_word_count == 0)
                         return "Positive";
                     else
                         return "Neutral";
@@ -78,6 +60,8 @@ public class ConsumerKafka {
                 .selectExpr("split(value, ',') as data")
             // .selectExpr("explode(split(data[5], ';')) as neg_rev", 
                 .selectExpr("cast(data[5] as string) as Hotel_Name",
+                              "cast(data[6] as string) as Reviewr_Nationality",
+                              "cast(data[13] as string) as Day_since_review",
                               "cast(data[8] as int) as neg_rev",
                               "cast(data[11] as int) as pos_rev");
             // .map((MapFunction) line -> {
@@ -86,54 +70,38 @@ public class ConsumerKafka {
         Dataset<Row> dfWithSentiment = df.withColumn("sentiment",
                 callUDF("sentimentAnalysis", col("neg_rev"), col("pos_rev")));
 
-        // dfWithSentiment.show();
 
+        String collectionName = "HotelsReviews";
 
-
-        String mongoConnectionString  = "mongodb://localhost:27017";
-        String mongoDatabaseName = "your-database-name";
-        String mongoCollectionName = "your-collection-name";
-
-
-        MongoClient mongoClient = MongoClients.create(mongoConnectionString);
-        MongoDatabase database = mongoClient.getDatabase(mongoDatabaseName);
-        MongoCollection<Document> collection = database.getCollection(mongoCollectionName);
-
-
-
-        // ConnectionString mongoURI = new ConnectionString(uri);
-        //             MongoClientSettings settings = MongoClientSettings.builder()
-        //                     .applyConnectionString(mongoURI)
-        //                     .build();
-        // MongoClient mongoClient = MongoClients.create(settings);
-        // MongoDatabase database = mongoClient.getDatabase("GlobalTerrorism");
-        // MongoCollection<Document> collection = database.getCollection("stream");
-        // Document doc = new Document("pairs", );
-        // collection.insertOne(doc);
-        // mongoClient.close();
-       
-
-     
-        // Start running the query that prints the artist with max streams and the corresponding track name
-        StreamingQuery query = dfWithSentiment.writeStream()
+    StreamingQuery query = dfWithSentiment.writeStream()
                 .outputMode("append")
-                .format("console")
-                // .foreachBatch((batchDF, batchId) -> {
-                //     // Write each batch to MongoDB
-                //     batchDF.write()
-                //             .format("mongodb")
-                //             //.option("checkpointLocation", "/tmp/")
-                //             //.option("forceDeleteTempCheckpointLocation", "true")
-                //             //.option("spark.mongodb.connection.uri", "mongodb://localhost:27017/")
-                //             .option("database", "BigData_Project")
-                //             .option("collection", "stream")
-                //             .mode("append")
-                //             .save();
-                // })
-                .trigger(Trigger.ProcessingTime("20 second"))
-                .start();
+                .trigger(Trigger.ProcessingTime("20 seconds"))
+                .foreachBatch((VoidFunction2<Dataset<Row>, Long>) (batchDF, batchId) -> {
+        // Process each batch here
+                    batchDF.persist(); // Cache the DataFrame for better performance if needed
 
-        query.awaitTermination();
+        // Example processing: Print the content of the batch DataFrame
+                    System.out.println("Batch ID: " + batchId);
+                    batchDF.show(false); // Show the content of the batch DataFrame
+        
+        
+                    
+
+        // Example processing: Write the batch data to MongoDB
+                batchDF.write()
+                    .format("com.mongodb.spark.sql.DefaultSource")
+                    .mode("append")
+                    .option("spark.mongodb.output.uri",  "mongodb://localhost:27017/Hotels." + collectionName)
+                    .save();
+        
+        // Unpersist the DataFrame to release memory
+        batchDF.unpersist();
+    })
+    .start();
+
+    query.awaitTermination();
+
+
     }
 }
 
